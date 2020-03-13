5b559c9fada079084d60bfa1b9bbb7c4ed3cdfa0
AnaHernandez <anav.hernandez@softtek.com>
2020-03-13 12:33:10 -0600

    Se agrego otra vez RecommendationService

--- RecommendationService/Controllers/RecommendationServiceController.cs
+++ RecommendationService/Controllers/RecommendationServiceController.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProductCatalogService.Models;
 
-namespace RecommendationService.Controllers
+namespace RecomendationService.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class RecommendationServiceController : ControllerBase
+    public class RecomendationServiceController : ControllerBase
     {
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult GetRecomendation(string id)
+        {
+            HttpClient recomendation = new HttpClient();
+            recomendation.BaseAddress = new Uri("https://localhost:44357/api/ProductCatalogService?pageNumber=1");
+
+            var response = recomendation.GetAsync("");
+            response.Wait();
+            var items = response.Result.Content.ReadAsAsync<PageDTO>();
+
+            return Ok();
+        }
     }
 }
